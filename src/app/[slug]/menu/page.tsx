8689896bb5fa15678a55438125f84a86ed8a2bc8
Victornck <193505785+Victornck@users.noreply.github.com>
2026-01-26 18:57:14 -0300

    feat: add restaurant header on menu page

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -4,6 +4,7 @@ import { db } from "@/lib/prisma";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import RestaurantHeader from "./componentes/header";
 
 interface RestaurantMenuPageProps {
   params: Promise<{ slug: string }>;
@@ -31,28 +32,7 @@ const RestaurantMenuPage = async ({
 
   return (
     <div>
-      <div className="relative h-[250px] w-full">
-        <Button
-          variant="secondary"
-          size="icon"
-          className="absolute left-4 z-50 rounded-full"
-        >
-          <ChevronLeftIcon />
-        </Button>
-        <Image
-          src={restaurant?.coverImageUrl}
-          alt={restaurant?.name}
-          fill
-          className="object-cover"
-        />
-        <Button
-          variant="secondary"
-          size="icon"
-          className="absolute right-4 z-50 rounded-full"
-        >
-          <ScrollTextIcon />
-        </Button>
-      </div>
+      <RestaurantHeader restaurant={restaurant} />
     </div>
   );
 };
